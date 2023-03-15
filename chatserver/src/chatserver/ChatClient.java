00c1be4ae394efc13efc3df57f95186bb2dfe72c
OmeshSachethana <sachethana12@gmail.com>
2023-03-15 15:12:16 +0530

    Updated

--- chatserver/src/chatserver/ChatClient.java
+++ chatserver/src/chatserver/ChatClient.java
@@ -1,6 +1,6 @@
 package chatserver;
 
-
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.BufferedReader;
@@ -21,69 +21,79 @@ import javax.swing.JTextField;
 import javax.swing.ListModel;
 
 /**
- * A simple Swing-based client for the chat server.  Graphically
- * it is a frame with a text field for entering messages and a
- * textarea to see the whole dialog.
+ * A simple Swing-based client for the chat server. Graphically it is a frame
+ * with a text field for entering messages and a textarea to see the whole
+ * dialog.
  *
- * The client follows the Chat Protocol which is as follows.
- * When the server sends "SUBMITNAME" the client replies with the
- * desired screen name.  The server will keep sending "SUBMITNAME"
- * requests as long as the client submits screen names that are
- * already in use.  When the server sends a line beginning
- * with "NAMEACCEPTED" the client is now allowed to start
- * sending the server arbitrary strings to be broadcast to all
- * chatters connected to the server.  When the server sends a
- * line beginning with "MESSAGE " then all characters following
- * this string should be displayed in its message area.
+ * The client follows the Chat Protocol which is as follows. When the server
+ * sends "SUBMITNAME" the client replies with the desired screen name. The
+ * server will keep sending "SUBMITNAME" requests as long as the client submits
+ * screen names that are already in use. When the server sends a line beginning
+ * with "NAMEACCEPTED" the client is now allowed to start sending the server
+ * arbitrary strings to be broadcast to all chatters connected to the server.
+ * When the server sends a line beginning with "MESSAGE " then all characters
+ * following this string should be displayed in its message area.
  */
 public class ChatClient {
 
-    BufferedReader in;
-    PrintWriter out;
-    JFrame frame = new JFrame("Chatter");
-    JTextField textField = new JTextField(40);
-    JTextArea messageArea = new JTextArea(8, 40);
-    // TODO: Add a list box
+	BufferedReader in;
+	PrintWriter out;
+	JFrame frame = new JFrame("Chatter");
+	JTextField textField = new JTextField(40);
+	JTextArea messageArea = new JTextArea(8, 40);
 
-    JCheckBox checkBox = new JCheckBox("Broadcast");
-    
-    // Add list to show client list 
+	// TODO: Add a list box
+	/* Add checkbox for Broadcast feature */
+	JCheckBox checkBox = new JCheckBox("Broadcast");
+	
+	/* Add list to show client list */
 	JList userlist = new JList();
-
-    // To store clients
+	
+	/* For store clients */
 	HashSet<String> selectedClient = new HashSet<String>();
 	DefaultListModel listModel;
 	
-	// To handle checkbox
-	static boolean checkBoxcheck = true;
+	/* For checkbox handle */
+	static boolean checkBoxcheck = true;	
+	
 
-    /**
-     * Constructs the client by laying out the GUI and registering a
-     * listener with the textfield so that pressing Return in the
-     * listener sends the textfield contents to the server.  Note
-     * however that the textfield is initially NOT editable, and
-     * only becomes editable AFTER the client receives the NAMEACCEPTED
-     * message from the server.
-     */
-    public ChatClient() {
+	/**
+	 * Constructs the client by laying out the GUI and registering a listener with
+	 * the textfield so that pressing Return in the listener sends the textfield
+	 * contents to the server. Note however that the textfield is initially NOT
+	 * editable, and only becomes editable AFTER the client receives the
+	 * NAMEACCEPTED message from the server.
+	 */
+	public ChatClient() {
 
-        // Layout GUI
-        textField.setEditable(false);
-        messageArea.setEditable(false);
-        frame.getContentPane().add(textField, "North");
-        frame.getContentPane().add(new JScrollPane(messageArea), "Center");
-        frame.pack();
+		// Layout GUI
+		textField.setEditable(false);
+		messageArea.setEditable(false);
 
-        // TODO: You may have to edit this event handler to handle point to point messaging,
-        // where one client can send a message to a specific client. You can add some header to 
-        // the message to identify the recipient. You can get the receipient name from the listbox.
-        textField.addActionListener(new ActionListener() {
-            /**
-             * Responds to pressing the enter key in the textfield by sending
-             * the contents of the text field to the server.    Then clear
-             * the text area in preparation for the next message.
-             */
-            public void actionPerformed(ActionEvent e) {
+		/* change GUI for new features */
+		frame.getContentPane().add(textField, BorderLayout.NORTH);
+		frame.getContentPane().add(checkBox, BorderLayout.WEST);
+		frame.getContentPane().add(new JScrollPane(messageArea), BorderLayout.SOUTH);
+		frame.getContentPane().add(new JScrollPane(userlist), BorderLayout.CENTER);
+		
+		/* checkbox checked */
+		checkBox.setSelected(true);
+
+		frame.pack();
+
+		// TODO: You may have to edit this event handler to handle point to point
+		// messaging,
+		// where one client can send a message to a specific client. You can add some
+		// header to
+		// the message to identify the recipient. You can get the receipient name from
+		// the listbox.
+		textField.addActionListener(new ActionListener() {
+			/**
+			 * Responds to pressing the enter key in the textfield by sending the contents
+			 * of the text field to the server. Then clear the text area in preparation for
+			 * the next message.
+			 */
+			public void actionPerformed(ActionEvent e) {
 				
 				/* check whether is there any selected client in the list.
 				 * then store in 'selectedClient'.
@@ -130,38 +140,28 @@ public class ChatClient {
 				}
 			}
 		});
-        
-        
-    }
+	}
 
-    /**
-     * Prompt for and return the address of the server.
-     */
-    private String getServerAddress() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Enter IP Address of the Server:",
-            "Welcome to the Chatter",
-            JOptionPane.QUESTION_MESSAGE);
-    }
+	/**
+	 * Prompt for and return the address of the server.
+	 */
+	private String getServerAddress() {
+		return JOptionPane.showInputDialog(frame, "Enter IP Address of the Server:", "Welcome to the Chatter", JOptionPane.QUESTION_MESSAGE);
+	}
 
-    /**
-     * Prompt for and return the desired screen name.
-     */
-    private String getName() {
-        return JOptionPane.showInputDialog(
-            frame,
-            "Choose a screen name:",
-            "Screen name selection",
-            JOptionPane.PLAIN_MESSAGE);
-    }
+	/**
+	 * Prompt for and return the desired screen name.
+	 */
+	private String getName() {
+		return JOptionPane.showInputDialog(frame, "Choose a screen name:", "Screen name selection", JOptionPane.PLAIN_MESSAGE);
+	}
 
-    /**
-     * Connects to the server then enters the processing loop.
-     */
-    private void run() throws IOException {
+	/**
+	 * Connects to the server then enters the processing loop.
+	 */
+	private void run() throws IOException {
 
-        // Make connection and initialize streams
+		// Make connection and initialize streams
 		String serverAddress = getServerAddress();
 		Socket socket = new Socket(serverAddress, 9001);
 		in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -202,15 +202,15 @@ public class ChatClient {
 				messageArea.append(line.substring(8) + "\n");
 			}
 		}
-    }
+	}
 
-    /**
-     * Runs the client as an application with a closeable frame.
-     */
-    public static void main(String[] args) throws Exception {
-        ChatClient client = new ChatClient();
-        client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        client.frame.setVisible(true);
-        client.run();
-    }
+	/**
+	 * Runs the client as an application with a closeable frame.
+	 */
+	public static void main(String[] args) throws Exception {
+		ChatClient client = new ChatClient();
+		client.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		client.frame.setVisible(true);
+		client.run();
+	}
 }
--- chatserver/src/chatserver/ChatServer.java
+++ chatserver/src/chatserver/ChatServer.java
@@ -1,6 +1,5 @@
 package chatserver;
 
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -11,137 +10,134 @@ import java.util.HashMap;
 import java.util.HashSet;
 
 /**
- * A multithreaded chat room server.  When a client connects the
- * server requests a screen name by sending the client the
- * text "SUBMITNAME", and keeps requesting a name until
- * a unique one is received.  After a client submits a unique
- * name, the server acknowledges with "NAMEACCEPTED".  Then
- * all messages from that client will be broadcast to all other
- * clients that have submitted a unique screen name.  The
- * broadcast messages are prefixed with "MESSAGE ".
+ * A multithreaded chat room server. When a client connects the server requests
+ * a screen name by sending the client the text "SUBMITNAME", and keeps
+ * requesting a name until a unique one is received. After a client submits a
+ * unique name, the server acknowledges with "NAMEACCEPTED". Then all messages
+ * from that client will be broadcast to all other clients that have submitted a
+ * unique screen name. The broadcast messages are prefixed with "MESSAGE ".
  *
- * Because this is just a teaching example to illustrate a simple
- * chat server, there are a few features that have been left out.
- * Two are very useful and belong in production code:
+ * Because this is just a teaching example to illustrate a simple chat server,
+ * there are a few features that have been left out. Two are very useful and
+ * belong in production code:
  *
- *     1. The protocol should be enhanced so that the client can
- *        send clean disconnect messages to the server.
+ * 1. The protocol should be enhanced so that the client can send clean
+ * disconnect messages to the server.
  *
- *     2. The server should do some logging.
+ * 2. The server should do some logging.
  */
 public class ChatServer {
 
-    /**
-     * The port that the server listens on.
-     */
-    private static final int PORT = 9001;
+	 //The port that the server listens on.
+	private static final int PORT = 9001;
 
-    /**
-     * The set of all names of clients in the chat room.  Maintained
-     * so that we can check that new clients are not registering name
-     * already in use.
-     */
-    private static HashSet<String> names = new HashSet<String>();
+	/**
+	 * The set of all names of clients in the chat room. Maintained so that we can
+	 * check that new clients are not registering name already in use.
+	 */
+	private static HashSet<String> names = new HashSet<String>();
 
-    /**
-     * The set of all the print writers for all the clients.  This
-     * set is kept so we can easily broadcast messages.
-     */
-    private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	/**
+	 * The set of all the print writers for all the clients. This set is kept so we
+	 * can easily broadcast messages.
+	 */
+	private static HashSet<PrintWriter> writers = new HashSet<PrintWriter>();
+	
+	/* HashSet variable for selected client */
+	private static HashSet<String> selectedClientNames = new HashSet<String>();	
 
-    /**
-     * The appplication main method, which just listens on a port and
-     * spawns handler threads.
-     */
+	/* Fore store client names and PrintWriter */
+	private static HashMap<String, PrintWriter> clientNameWithPrintWriter = new HashMap<String, PrintWriter>();
 
-    private static HashSet<String> selectedClientNames = new HashSet<>();
+	/**
+	 * The appplication main method, which just listens on a port and spawns handler
+	 * threads.
+	 */
+	public static void main(String[] args) throws Exception {
+		System.out.println("The chat server is running.");
+		ServerSocket listener = new ServerSocket(PORT);
+		try {
+			while (true) {
+				
+				Socket socket = listener.accept();
 
-    //created a hashmap
-    private static HashMap<String, PrintWriter> clientNameWithPrintWriter = new HashMap<>();
+				Thread handlerThread = new Thread(new Handler(socket));
+				handlerThread.start();
+				System.out.println("New client connected!");
+			}
+		} finally {
+			listener.close();
+		}
+	}
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("The chat server is running.");
-        ServerSocket listener = new ServerSocket(PORT);
-        try {
-            while (true) {
-            	Socket socket  = listener.accept();
-                Thread handlerThread = new Thread(new Handler(socket));
-                handlerThread.start();
-            }
-        } finally {
-            listener.close();
-        }
-    }
+	/**
+	 * A handler thread class. Handlers are spawned from the listening loop and are
+	 * responsible for a dealing with a single client and broadcasting its messages.
+	 */
+	private static class Handler implements Runnable {
+		private String name;
+		private Socket socket;
+		private BufferedReader in;
+		private PrintWriter out;
 
-    /**
-     * A handler thread class.  Handlers are spawned from the listening
-     * loop and are responsible for a dealing with a single client
-     * and broadcasting its messages.
-     */
-    private static class Handler implements Runnable {
-        private String name;
-        private Socket socket;
-        private BufferedReader in;
-        private PrintWriter out;
+		/**
+		 * Constructs a handler thread, squirreling away the socket. All the interesting
+		 * work is done in the run method.
+		 */
+		public Handler(Socket socket) {
+			this.socket = socket;
+		}
 
-        /**
-         * Constructs a handler thread, squirreling away the socket.
-         * All the interesting work is done in the run method.
-         */
-        public Handler(Socket socket) {
-            this.socket = socket;
-        }
+		/**
+		 * Services this thread's client by repeatedly requesting a screen name until a
+		 * unique one has been submitted, then acknowledges the name and registers the
+		 * output stream for the client in a global set, then repeatedly gets inputs and
+		 * broadcasts them.
+		 */
+		public void run() {
+			try {
 
-        /**
-         * Services this thread's client by repeatedly requesting a
-         * screen name until a unique one has been submitted, then
-         * acknowledges the name and registers the output stream for
-         * the client in a global set, then repeatedly gets inputs and
-         * broadcasts them.
-         */
-        public void run() {
-            try {
+				// Create character streams for the socket.
+				in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				out = new PrintWriter(socket.getOutputStream(), true);
 
-                // Create character streams for the socket.
-                in = new BufferedReader(new InputStreamReader(
-                    socket.getInputStream()));
-                out = new PrintWriter(socket.getOutputStream(), true);
+				// Request a name from this client. Keep requesting until
+				// a name is submitted that is not already used. Note that
+				// checking for the existence of a name and adding the name
+				// must be done while locking the set of names.
+				while (true) {
+					out.println("SUBMITNAME");
+					name = in.readLine();
+					if (name == null) {
+						return;
+					}
 
-                // Request a name from this client.  Keep requesting until
-                // a name is submitted that is not already used.  Note that
-                // checking for the existence of a name and adding the name
-                // must be done while locking the set of names.
-                while (true) {
-                    out.println("SUBMITNAME");
-                    name = in.readLine();
-                    if (name == null) {
-                        return;
-                    }
-                    
-                    // TODO: Add code to ensure the thread safety of the
-                    // the shared variable 'names'
-                    synchronized (names) {
-                        if (!names.contains(name)) {
-                                names.add(name);
-                                break;
-                            }
-                    }
-                 }
+					// TODO: Add code to ensure the thread safety of the
+					// the shared variable 'names'
 
-                // Now that a successful name has been chosen, add the
-                // socket's print writer to the set of all writers so
-                // this client can receive broadcast messages.
-                out.println("NAMEACCEPTED");
-                writers.add(out);
-                
-                // TODO: You may have to add some code here to broadcast all clients the new
-                // client's name for the task 9 on the lab sheet. 
-                
-                //add client details to hashmap
-                clientNameWithPrintWriter.put(name, out);
-                
+					synchronized (names) {
+						if (!names.contains(name)) {
+							names.add(name);
+							break;
+						}
+					}
+				}
 
-                for (String key : clientNameWithPrintWriter.keySet()) {
+				// Now that a successful name has been chosen, add the
+				// socket's print writer to the set of all writers so
+				// this client can receive broadcast messages.
+				out.println("NAMEACCEPTED");
+				writers.add(out);
+
+				// TODO: You may have to add some code here to broadcast all clients the new
+				// client's name for the task 9 on the lab sheet.
+				
+				/* adding client name to hashMap */ 
+				clientNameWithPrintWriter.put(name, out);
+				
+				/* When client added to the server, client's name send to existing client's list.
+				 * And existing clients name to new client's list. */
+				for (String key : clientNameWithPrintWriter.keySet()) {
 
 					if (key.equals(name)) {
 						for (String name : names) {
@@ -152,9 +148,13 @@ public class ChatServer {
 					}
 				}
 
-                // Accept messages from this client and broadcast them.
-                // Ignore other clients that cannot be broadcasted to.
-                while (true) {
+				// Accept messages from this client and broadcast them.
+				// Ignore other clients that cannot be broadcasted to.
+				
+				// TODO: Add code to send a message to a specific client and not
+				// all clients. You may have to use a HashMap to store the sockets along
+				// with the chat client names
+				while (true) {
 
 					String input = in.readLine();
 					
@@ -173,7 +173,7 @@ public class ChatServer {
 							}
 						}
 
-						/* clear selectedtClientNames for accept new. */
+						/* clear selectedClientNames for accept new. */
 						selectedClientNames.clear();
 
 					/* If get message with 'CHECK'. Then send message to all clients */
@@ -186,27 +186,32 @@ public class ChatServer {
 						selectedClientNames.add(input);
 					}
 				}
-            }// TODO: Handle the SocketException here to handle a client closing the socket
-            catch (java.net.SocketException e) {
-				System.out.println("client left.");
 
+			} // TODO: Handle the SocketException here to handle a client closing the socket
+			catch (java.net.SocketException e) {
+				System.out.println("Seems one client left.");
+
+			} catch (IOException e) {
+				System.out.println(e);
+			} finally {
+				// This client is going down! Remove its name and its print
+				// writer from the sets, and close its socket.
+				if (name != null) {
+					names.remove(name);
+
+					/* When client left the server, remove client from the HashMap. */
+					clientNameWithPrintWriter.remove(name);
+
+					if (out != null) {
+						writers.remove(out);
+					}
+
+					try {
+						socket.close();
+					} catch (IOException e) {
+					}
+				}
 			}
-            catch (IOException e) {
-                System.out.println(e);
-            } finally {
-                // This client is going down!  Remove its name and its print
-                // writer from the sets, and close its socket.
-                if (name != null) {
-                    names.remove(name);
-                }
-                if (out != null) {
-                    writers.remove(out);
-                }
-                try {
-                    socket.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-    }
+		}
+	}
 }
